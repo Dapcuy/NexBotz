971fd62eb5d41bbb64f0572c0682774c72c549aa
Dapcuy <118491026+Dapcuy@users.noreply.github.com>
2023-01-30 18:58:48 +0700

    Update anime-kaguya.js

--- plugins/anime-kaguya.js
+++ plugins/anime-kaguya.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://web-production-7c28.up.railway.app/api/wallpaper/shinomiya?apikey=APIKEY'
+	let url = 'https://revita.herokuapp.com/api/wallpaper/shinomiya?apikey=ApiRevita'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(shinomiya|kaguya)$/i
