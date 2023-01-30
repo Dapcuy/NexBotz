32a5e74724d9dd213bada179c85f23a5d53eb8f1
Dapcuy <118491026+Dapcuy@users.noreply.github.com>
2023-01-30 18:55:13 +0700

    Update anime-elaina.js

--- plugins/anime-elaina.js
+++ plugins/anime-elaina.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://web-production-7c28.up.railway.app/api/wallpaper/elaina?apikey=APIKEY'
+	let url = 'https://raw.githubusercontent.com/KazukoGans/database/main/anime/elaina.json'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(elaina)$/i
