a5042f8835f265c9f26a6594d747af0d77631e9b
Dapcuy <118491026+Dapcuy@users.noreply.github.com>
2023-01-30 18:28:56 +0700

    Update anime-asuna.js

--- plugins/anime-asuna.js
+++ plugins/anime-asuna.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://web-production-7c28.up.railway.app/api/wallpaper/asuna?apikey=APIKEY'
+	let url = 'https://api.zeeoneofc.xyz/api/anime/asuna-yuki?apikey=3JE7TvJt'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(asuna)$/i
