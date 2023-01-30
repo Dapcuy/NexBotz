bad7f4a6f7d789ed0405d404a797bf66d64bd95b
Dapcuy <118491026+Dapcuy@users.noreply.github.com>
2023-01-30 18:21:21 +0700

    Update anime-anna.js

--- plugins/anime-anna.js
+++ plugins/anime-anna.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://web-production-7c28.up.railway.app/api/wallpaper/ana?apikey=APIKEY'
+	let url = 'https://api.zeeoneofc.xyz/api/anime/anna?apikey=5Cd8U3tG'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(anna)$/i
