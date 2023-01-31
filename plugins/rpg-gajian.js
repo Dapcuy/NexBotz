ab76b69ea042f265875214c02df85cefadbb5f22
Dapcuy <118491026+Dapcuy@users.noreply.github.com>
2023-01-31 19:52:30 +0700

    Update rpg-gajian.js

--- plugins/rpg-gajian.js
+++ plugins/rpg-gajian.js
@@ -4,10 +4,10 @@ let cdm = `${MeNit(new Date - LastClaim)}`
 let cds = `${DeTik(new Date - LastClaim)}`
 let cd1 = Math.ceil(44 - cdm)
 let cd2 = Math.ceil(59 - cds)
-  if (new Date - global.db.data.users[m.sender].lastclaim > 2700000) {
-    global.db.data.users[m.sender].uang += 7000
-    global.db.data.users[m.sender].exp += 100
-    m.reply('Nih gaji lu +Rp7000')
+ if (new Date - global.db.data.users[m.sender].lastclaim > 2700000) {
+    global.db.data.users[m.sender].uang += Math.floor(Math.random() * (10000 - 5000 + 1) + 5000);
+    global.db.data.users[m.sender].exp += Math.floor(Math.random() * (150 - 50 + 1) + 50);
+    m.reply(`Nih gaji lu +Rp${global.db.data.users[m.sender].uang}`)
     global.db.data.users[m.sender].lastclaim = new Date * 1
   } else throw `Lu udah ambil jatah hari ini.\n\nTunggu ${cd1} Menit ${cd2} Detik!`
 }
